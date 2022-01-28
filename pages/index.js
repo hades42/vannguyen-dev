dd8e05ed6fad76ae6263a2158d6705c6153e6907
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-01-28 11:27:46 +1100

    Add profile picture

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,40 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Image } from '@chakra-ui/react';
 
 const Page = () => {
   return (
     <Container>
       <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
-        Hello, I'm just a geek who love making software
+        Hi, I'm just a geek who love making software
       </Box>
 
-      <Box display={{ md: 'flex' }}></Box>
-      <Box flexGrow={1}>
-        <Heading as="h2" variant="page-title">
-          Van Nguyen Nguyen
-        </Heading>
-        <p>Software Engineering (Developer / Designer)</p>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Van Nguyen Nguyen
+          </Heading>
+          <p>Software Engineering (Developer / Designer)</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          align="center"
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            objectFit="cover"
+            src="/images/profile.png"
+            alt="Profile Image"
+          ></Image>
+        </Box>
       </Box>
     </Container>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;

207a8b1e359e589db7331883d1f4cc31fcdd2dc6
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-01-29 01:27:36 +1100

    Add Project page

---
+++ pages/projects.js
@@ -0,0 +1,14 @@
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react';
+import Section from '../components/section';
+
+const Projects = () => {
+  return (
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Projects
+      </Heading>
+    </Container>
+  );
+};
+
+export default Projects;

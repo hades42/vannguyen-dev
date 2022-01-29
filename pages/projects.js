f5795ca6dadc4f8418d27301ac1dc3d38a9f1d7c
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-01-29 15:48:40 +1100

    Add Projects section

--- pages/projects.js
+++ pages/projects.js
@@ -1,12 +1,37 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react';
 import Section from '../components/section';
+import { ProjectGridItem } from '../components/grid-item';
+import shopThumbNail from '../public/images/shopology-front.png';
+import mdSocial from '../public/images/MDEditor.png';
 
 const Projects = () => {
   return (
     <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
+      <Heading as="h3" fontSize={20} mb={4} mt={6}>
         Projects
       </Heading>
+
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section>
+          <ProjectGridItem
+            id="shopology"
+            title="Shopology"
+            thumbnail={shopThumbNail}
+          >
+            An E-commerce Web application built with MERN stack
+          </ProjectGridItem>
+        </Section>
+        <Section>
+          <ProjectGridItem
+            id="markdown-social"
+            title="MDSocial"
+            thumbnail={mdSocial}
+          >
+            A web application for people to share their post in markdown format
+            will many features supported text editor
+          </ProjectGridItem>
+        </Section>
+      </SimpleGrid>
     </Container>
   );
 };

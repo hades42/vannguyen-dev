b7f5c172f36ce0ce9cfb7240ddd7dc39b289da3c
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-01-30 22:59:18 +1100

    Add animation component

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,16 @@ import { ChakraProvider } from '@chakra-ui/react';
 import Main from '../components/layout/main';
 import theme from '../lib/theme';
 import Fonts from '../components/fonts';
+import { AnimatePresence } from 'framer-motion';
 
 const Website = ({ Component, pageProps, router }) => {
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
       <Main router={router}>
-        <Component {...pageProps} key={router.route} />
+        <AnimatePresence exitBeforeEnter initial={true}>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
       </Main>
     </ChakraProvider>
   );

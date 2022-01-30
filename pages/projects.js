ffcfa16536535cb7586e4f4a3596d0d6312b4f42
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-01-30 22:58:46 +1100

    Edit the Projects page

--- pages/projects.js
+++ pages/projects.js
@@ -3,36 +3,39 @@ import Section from '../components/section';
 import { ProjectGridItem } from '../components/grid-item';
 import shopThumbNail from '../public/images/shopology-front.png';
 import mdSocial from '../public/images/MDEditor.png';
+import Layout from '../components/layout/article';
 
 const Projects = () => {
   return (
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4} mt={6}>
-        Projects
-      </Heading>
+    <Layout>
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4} mt={6}>
+          Projects
+        </Heading>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-          <ProjectGridItem
-            id="shopology"
-            title="Shopology"
-            thumbnail={shopThumbNail}
-          >
-            An E-commerce Web application built with MERN stack
-          </ProjectGridItem>
-        </Section>
-        <Section>
-          <ProjectGridItem
-            id="markdown-social"
-            title="MDSocial"
-            thumbnail={mdSocial}
-          >
-            A web application for people to share their post in markdown format
-            will many features supported text editor
-          </ProjectGridItem>
-        </Section>
-      </SimpleGrid>
-    </Container>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <ProjectGridItem
+              id="shopology"
+              title="Shopology"
+              thumbnail={shopThumbNail}
+            >
+              An E-commerce Web application built with MERN stack
+            </ProjectGridItem>
+          </Section>
+          <Section>
+            <ProjectGridItem
+              id="markdown-social"
+              title="MDSocial"
+              thumbnail={mdSocial}
+            >
+              A web application for people to share their post in markdown
+              format with a online text editor
+            </ProjectGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   );
 };
 

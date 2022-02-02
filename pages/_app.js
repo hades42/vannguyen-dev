b5bd81e923c8f46cffedd3d4fe2abb39641763d6
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-02-03 00:36:09 +1100

    Wrapping MDXProvider to apply custom component to the app

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,33 @@ import Main from '../components/layout/main';
 import theme from '../lib/theme';
 import Fonts from '../components/fonts';
 import { AnimatePresence } from 'framer-motion';
+import { Global, css } from '@emotion/react';
+import { MDXProvider } from '@mdx-js/react';
+import CustomCode from '../components/code-block/codeblock';
+import codeTheme from '../components/code-block/theme';
+
+const components = { pre: CustomCode };
+
+const CodeTheme = () => {
+  return (
+    <Global
+      styles={css`
+        ${codeTheme}
+      `}
+    ></Global>
+  );
+};
 
 const Website = ({ Component, pageProps, router }) => {
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
+      <CodeTheme />
       <Main router={router}>
         <AnimatePresence exitBeforeEnter initial={true}>
-          <Component {...pageProps} key={router.route} />
+          <MDXProvider components={components}>
+            <Component {...pageProps} key={router.route} />
+          </MDXProvider>
         </AnimatePresence>
       </Main>
     </ChakraProvider>

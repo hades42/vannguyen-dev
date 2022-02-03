f3f47b1c4e01fba4f76e781cf208e0e997382ac6
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-02-04 03:20:05 +1100

    Modify margin for MyList and MyHr

--- components/mdx/custom-component.js
+++ components/mdx/custom-component.js
@@ -2,10 +2,10 @@ import { Heading, Text, Image, useColorModeValue } from '@chakra-ui/react';
 import { Reference } from '../paragraph';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 
-const MyH1 = props => <Heading as="h1" size="lg" {...props} />;
-const MyH2 = props => <Heading as="h2" size="md" {...props} />;
-const MyH3 = props => <Heading as="h3" size="sm" {...props} />;
-const MyH4 = props => <Heading as="h4" size="xs" {...props} />;
+const MyH1 = props => <Heading marginY="18px" as="h1" size="lg" {...props} />;
+const MyH2 = props => <Heading marginY="18px" as="h2" size="md" {...props} />;
+const MyH3 = props => <Heading marginY="18px" as="h3" size="sm" {...props} />;
+const MyH4 = props => <Heading marginY="18px" as="h4" size="xs" {...props} />;
 
 const MyLink = props => {
   let content = props.children;
@@ -40,12 +40,12 @@ const MyImage = props => {
 };
 
 const MyList = props => {
-  return <li style={{ marginLeft: '30px' }}>{props.children}</li>;
+  return <li style={{ margin: '10px 0 10px 30px' }}>{props.children}</li>;
 };
 
 const MyHr = props => {
   const color = useColorModeValue('black', '#4a4a49');
-  return <hr style={{ borderColor: `${color}`, margin: '50px 0' }} />;
+  return <hr style={{ borderColor: `${color}`, margin: '30px 0' }} />;
 };
 
 export { MyH1, MyH4, MyH3, MyH2, MyLink, MyStrong, MyImage, MyList, MyHr };

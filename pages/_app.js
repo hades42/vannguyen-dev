f6742af5a3c1160fa94c5b4d9dfd6c295d3ff658
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-02-10 18:48:54 +1100

    Remove MDXProvider from the whole app

--- pages/_app.js
+++ pages/_app.js
@@ -5,33 +5,7 @@ import theme from '../lib/theme';
 import Fonts from '../components/fonts';
 import { AnimatePresence } from 'framer-motion';
 import { Global, css } from '@emotion/react';
-import { MDXProvider } from '@mdx-js/react';
-import CustomCode from '../components/code-block/codeblock';
 import codeTheme from '../components/code-block/theme';
-import {
-  MyH1,
-  MyH2,
-  MyH3,
-  MyH4,
-  MyLink,
-  MyStrong,
-  MyImage,
-  MyList,
-  MyHr,
-} from '../components/mdx/custom-component';
-
-const components = {
-  pre: CustomCode,
-  h1: MyH1,
-  h2: MyH2,
-  h3: MyH3,
-  h4: MyH4,
-  a: MyLink,
-  strong: MyStrong,
-  img: MyImage,
-  li: MyList,
-  hr: MyHr,
-};
 
 const CodeTheme = () => {
   return (
@@ -50,9 +24,7 @@ const Website = ({ Component, pageProps, router }) => {
       <CodeTheme />
       <Main router={router}>
         <AnimatePresence exitBeforeEnter initial={true}>
-          <MDXProvider components={components}>
-            <Component {...pageProps} key={router.route} />
-          </MDXProvider>
+          <Component {...pageProps} key={router.route} />
         </AnimatePresence>
       </Main>
     </ChakraProvider>

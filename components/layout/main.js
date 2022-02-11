052b3de871a46316bc1ad1c6544e26163be2db52
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-02-11 17:11:53 +1100

    Remove useless <Head/>

--- components/layout/main.js
+++ components/layout/main.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Navbar from '../navbar';
 import { Box, Container } from '@chakra-ui/react';
 import Footer from '../footer';
@@ -6,14 +5,6 @@ import Footer from '../footer';
 const Main = ({ children, router }) => {
   return (
     <Box as="main">
-      <Head>
-        <link rel="icon" href="/images/favicon.ico"></link>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        ></meta>
-        <title>Van Nguyen Nguyen - Homepage</title>
-      </Head>
       <Navbar path={router.asPath} />
       <Container minH="90vh" maxW="container.md" pt={14}>
         {children}

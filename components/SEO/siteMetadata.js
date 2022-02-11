3eea2f782539854c79d58c34057523ac0b3e66c2
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-02-11 17:10:24 +1100

    Create SEO components

---
+++ components/SEO/siteMetadata.js
@@ -0,0 +1,17 @@
+export const siteMetadata = {
+  title: 'Van Nguyen Dev',
+  author: 'Van Nguyen Nguyen',
+  avatar: 'https://i.postimg.cc/18TrqfZF/avatar.jpg',
+  occupation: 'Software Engineer',
+  headerTitle: 'Van Nguyen Dev',
+  description:
+    'I am a software engineer who have a huge passion for building and making software better',
+  language: 'en-us',
+  siteUrl: 'https://www.vannguyen-dev.com',
+  siteLogo: '/images/favicon.ico',
+  socialBanner: '/images/avatar.jpg',
+  github: 'https://github.com/hades42',
+  twitter: 'https://twitter.com/NguynVn27052749',
+  linkedin: 'https://www.linkedin.com/in/van-nguyen-nguyen-031205178/',
+  locale: 'en-US',
+};

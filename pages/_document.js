e0029681e17c180ba91dceed81d622168dcf3975
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-02-11 17:14:11 +1100

    Adding proper metadata for icon

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,32 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons//favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <meta
+            name="msapplication-config"
+            content="/favicons/browserconfig.xml"
+          />
+          <meta name="msapplication-TileColor" content="#A3E635" />
+          <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />

a2f7419130ca573b4ce8a46915fdea2c3bc7462c
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-02-14 00:27:35 +1100

    Add loading component into main page

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,9 @@ import codeTheme from '../components/code-block/theme';
 import { DefaultSeo } from 'next-seo';
 import { SEO } from '../components/SEO/SEO';
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
+import { Router } from 'next/router';
+import Loading from '../components/Loading';
 
 const CodeTheme = () => {
   return (
@@ -21,6 +24,24 @@ const CodeTheme = () => {
 };
 
 const Website = ({ Component, pageProps, router }) => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+    const end = () => {
+      setLoading(false);
+    };
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+    };
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
       <Head>
@@ -33,9 +54,13 @@ const Website = ({ Component, pageProps, router }) => {
       <Fonts />
       <CodeTheme />
       <Main router={router}>
-        <AnimatePresence exitBeforeEnter initial={true}>
-          <Component {...pageProps} key={router.route} />
-        </AnimatePresence>
+        {loading ? (
+          <Loading />
+        ) : (
+          <AnimatePresence exitBeforeEnter initial={true}>
+            <Component {...pageProps} key={router.route} />
+          </AnimatePresence>
+        )}
       </Main>
     </ChakraProvider>
   );

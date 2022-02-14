1354c8149121c1cc6308dfaf12ade2922a57a7e6
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-02-14 17:38:11 +1100

    Fix color for 404 page

--- pages/404.js
+++ pages/404.js
@@ -15,12 +15,12 @@ const NotFound = () => {
       <Heading as="h1">Not Found</Heading>
       <Text>The page you are looking for was not found.</Text>
       <Divider
-        borderColor={useColorModeValue('purple', 'orange')}
+        borderColor={useColorModeValue('blackAlpha', 'orange')}
         my={6}
       ></Divider>
       <Box my={6} align="center">
-        <NextLink href="/">
-          <Button colorScheme={useColorModeValue('purple', 'orange')}>
+        <NextLink href="/" passHref>
+          <Button colorScheme={useColorModeValue('blackAlpha', 'orange')}>
             Return to home
           </Button>
         </NextLink>

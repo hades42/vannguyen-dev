c2e1ff25069f2b5cc3b1afdab48038b2c4c099a0
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-02-14 23:30:55 +1100

    Use Preact for build production

--- next.config.js
+++ next.config.js
@@ -1,27 +1,26 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
 
   // Prefer loading of ES Modules over CommonJS
   experimental: { esmExternals: true },
   // Support MDX files as pages:
-  pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
+  pageExtensions: ['md', 'mdx', 'jsx', 'js'],
   // Support loading `.md`, `.mdx`:
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.mdx?$/,
-      use: [
-        // The default `babel-loader` used by Next:
-        options.defaultLoaders.babel,
-        {
-          loader: '@mdx-js/loader',
-          /** @type {import('@mdx-js/loader').Options} */
-          options: {
-            /* jsxImportSource: …, otherOptions… */
-          },
-        },
-      ],
-    });
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      //webpack aliase for Preact for production mode only
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
 
     return config;
   },
-};
+});

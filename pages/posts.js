df370c87df7204ce07b4aa6f663055aacabe82e2
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-02-16 16:20:37 +1100

    Fix box link in /posts page

--- pages/posts.js
+++ pages/posts.js
@@ -6,6 +6,7 @@ import {
   Divider,
   useColorModeValue,
   LinkOverlay,
+  LinkBox,
 } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { getSortedPostsData } from '../lib/post';
@@ -24,21 +25,19 @@ export async function getStaticProps() {
 
 const SinglePost = ({ post }) => (
   <>
-    <NextLink href={`/posts/${post.id}`} passHref>
-      <LinkOverlay cursor="pointer" mb={3}>
-        <Heading as="h3" fontSize={20}>
-          <NextLink href={`/posts/${post.id}`} passHref>
-            {post.title}
-          </NextLink>
-        </Heading>
-        <Date dateString={post.date} />
-        <Text color={useColorModeValue('black', 'gray.300')} marginTop="2">
-          <NextLink href={`/posts/${post.id}`} passHref>
-            {post.summary}
-          </NextLink>
-        </Text>
-      </LinkOverlay>
-    </NextLink>
+    <LinkBox as="article" cursor="pointer" mb={3}>
+      <Heading as="h3" fontSize={20}>
+        <NextLink href={`/posts/${post.id}`} passHref>
+          <LinkOverlay>{post.title}</LinkOverlay>
+        </NextLink>
+      </Heading>
+      <Date dateString={post.date} />
+      <Text color={useColorModeValue('black', 'gray.300')} marginTop="2">
+        <NextLink href={`/posts/${post.id}`} passHref>
+          <LinkOverlay>{post.summary}</LinkOverlay>
+        </NextLink>
+      </Text>
+    </LinkBox>
     <Divider
       borderColor={useColorModeValue('gray.400', 'gray.600')}
       my={6}
